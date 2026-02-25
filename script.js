dfd4f7db4139193c2e845fc7afcf274f81b7f736
тася <taisiashlemkevich@gmail.com>
2026-02-25 08:51:19 +0200

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const items = document.querySelectorAll("ul li");
-items[2].textContent = "Hello world!";
-
-function showName() {
-    document.getElementById("buttonName").textContent = "Шлемкевич Таїсія";
-}
