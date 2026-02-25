494c0003ad64cfb28311d05a7a34635f5c016a7e
тася <taisiashlemkevich@gmail.com>
2026-02-25 08:50:36 +0200

    Add files via upload

---
+++ LR.1/script.js
@@ -0,0 +1,6 @@
+const items = document.querySelectorAll("ul li");
+items[2].textContent = "Hello world!";
+
+function showName() {
+    document.getElementById("buttonName").textContent = "Шлемкевич Таїсія";
+}
